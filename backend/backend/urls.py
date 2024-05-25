90f69572596edb4d41d59612ca5a5b8c3e3375f1
KifayaShehadeh <125777469+KifayaShehadeh@users.noreply.github.com>
2024-05-25 15:55:38 +1000

    Update urls.py

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,20 +1,27 @@
+"""RhombusAI URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
 from django.contrib import admin
-from django.urls import path, include
-from django.http import HttpResponse
+from django.urls import path
+from django.urls import include
+from django.contrib import admin
+from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
-from django.shortcuts import redirect
-
-# Simple view function to handle the base URL
-def home_view(request):
-    return HttpResponse("Welcome to the Contacts App API! Visit /cotacts/ to see the contacts.")
-
-# Alternatively, redirect to the contacts API
-def home_view(request):
-    return redirect('/contacts/')
+from django.urls import include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('contacts/', include('contacts.urls')),
-    path('', home_view),  # Add this line to handle the base URL
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
