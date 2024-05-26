61b5b30df9f910cdc15c290f82f320a96d1b677f
Kifaya Shehadeh <z5421904@ad.unsw.edu.au>
2024-05-26 10:23:46 +1000

    logging

--- backend/contacts/views.py
+++ backend/contacts/views.py
@@ -2,10 +2,14 @@ from django.shortcuts import render
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 import requests
+import logging
+
+logger = logging.getLogger(__name__)
+
 
 @api_view(['GET'])
 def get_contacts(request):
+    logger.debug(f"Request headers: {request.headers}")
     response = requests.get('https://jsonplaceholder.typicode.com/users')
     contacts = response.json()
-    print(contacts)
     return Response(contacts)
