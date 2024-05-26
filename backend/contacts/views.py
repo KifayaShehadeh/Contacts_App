ee66b4bd872078214bef6c0a6ee70d213989abc6
Kifaya Shehadeh <z5421904@ad.unsw.edu.au>
2024-05-26 11:20:24 +1000

    response headers

--- backend/contacts/views.py
+++ backend/contacts/views.py
@@ -3,12 +3,23 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 import requests
 from django.views.decorators.csrf import csrf_exempt
+import logging
 
+# contacts/views.py
+logger = logging.getLogger(__name__)
 
-@csrf_exempt
-@api_view(['GET'])
+from django.http import HttpResponse
+
+@api_view(['GET', 'OPTIONS'])
 def get_contacts(request):
-    print(request.headers)  # Debug line to print request headers
+    if request.method == 'OPTIONS':
+        response = HttpResponse()
+        response['Allow'] = 'GET, OPTIONS'
+        response['Access-Control-Allow-Origin'] = '*'
+        response['Access-Control-Allow-Headers'] = 'Content-Type, Authorization'
+        return response
+
+    logger.debug(f"Request headers: {request.headers}")
     response = requests.get('https://jsonplaceholder.typicode.com/users')
     contacts = response.json()
     return Response(contacts)
